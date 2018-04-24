5c57ca6db50fbbb3a1096471f3fb6855e00b69c1
oanaelenaa <oana.elena.toma1995@gmail.com>
2018-04-24 18:38:39 +0300

    addedtestClass2

--- src/test/java/controller/ClientControllerTest.java
+++ src/test/java/controller/ClientControllerTest.java
@@ -1,9 +1,12 @@
 package controller;
+import com.sun.xml.internal.ws.developer.UsesJAXBContext;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestSuite;
+import model.Client;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
+import repository.DataManager;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -25,9 +28,45 @@ public class ClientControllerTest{
         assertEquals(null,c.AddClient("Oana","Mera 27", "123"));
 
     }
+    @Test
+    public void testAddInvalidClientIndex(){
+        ClientController c=new ClientController();
+        c.AddClient("OanaT","Mera 24", "12345");
+        Client client=new Client("OanaT","Mera 24", "12345");
+        assertEquals("Money to pay can't be less than 0!",c.AddClientIndex(client,2018,5,-3));
+        assertEquals("Month can't be 0 or less!",c.AddClientIndex(client,2018,-5,3));
+        assertEquals("Year is not valid",c.AddClientIndex(client,218,5,3));
+        Client client2=new Client("Elena","Mera 24", "12345");
+        assertEquals("Client does not exist!",c.AddClientIndex(client2,2018,5,3));
+        c.AddClientIndex(client,2018,5,10);
+        assertEquals("Monthly index already exists!",c.AddClientIndex(client,2018,5,3));
+    }
+
+    @Test
+    public void testAddValidClientIndex() {
+        ClientController c=new ClientController();
+        c.AddClient("OanaT","Mera 24", "12345");
+        Client client=new Client("OanaT","Mera 24", "12345");
+        assertEquals(null,c.AddClientIndex(client,2018,5,3));
+    }
+
 
     @Test
     public void testIntegrationAddClient(){
+        //top down approach
+        ClientController ctrl = new ClientController();
+        String result=ctrl.AddClient("OanaT","Mera 24", "12345");
+        assertEquals(null,result);
+        Client c=new Client("OanaT","Mera 24", "12345");
+        String resultAddInvoice=ctrl.AddClientIndex(c,2018, 3,12);
+        assertEquals(null,resultAddInvoice);
+        String r=ctrl.ListIssue(c);
+        assertEquals("Year:2018, Month:3, Penalty:0",r);
+        String fileClient = "client.txt";
+        String fileIssue = "issue.txt";
+        DataManager dataManager=new DataManager();
+        assertEquals (0,dataManager.getClientsList());
+
 
     }
 
