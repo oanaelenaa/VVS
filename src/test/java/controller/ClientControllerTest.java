eb2f8989ebb4161c513599965df1d673b69ffd74
oanaelenaa <oana.elena.toma1995@gmail.com>
2018-04-24 17:21:36 +0300

    addedtestClass

--- src/test/java/controller/ClientControllerTest.java
+++ src/test/java/controller/ClientControllerTest.java
@@ -1,16 +1,29 @@
 package controller;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestSuite;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
 
+import static junit.framework.TestCase.assertEquals;
+
 @RunWith(AllTests.class)
 public class ClientControllerTest{
+
+    @Test
     public void addInvalidClient(){
+        ClientController c=new ClientController();
+
+        assertEquals("Client already exists!", c.AddClient("Lorincz Arnold","Mera 278", "1"));
+        assertEquals("Name or address cannot be empty!", c.AddClient("","Mera 27", "123"));
+        assertEquals("Invalid character:@" , c.AddClient("@sa","Mera 27", "123"));
 
     }
 
+    @Test
     public void addValidClient(){
+        ClientController c=new ClientController();
+        assertEquals(null,c.AddClient("Oana","Mera 27", "123"));
 
     }
 
